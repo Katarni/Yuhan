d12a970769c872ffcd4545dc4ea080c1db2a3d5e
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-10-23 21:45:33 +0300

    added Type Literal & created generateWhat func

--- include/Token.h
+++ include/Token.h
@@ -12,62 +12,21 @@ class Token : public std::exception {
     enum class Type {
         ReservedWord, Identifier,
         Operator, Dot,
-        Comma, Punctuation
+        Comma, Punctuation,
+        Literal
     };
 
     Token(const std::string &content, size_t line, size_t col, Type type) : std::exception(),
                                                                             line_(line), column_(col),
                                                                             type_(type), content_(content) {
-        what_ = "in " + std::to_string(line_) + ":" + std::to_string(column_);
-        what_ += " error: unexpected token: \"" + content_ + "\" with type: ";
-        switch (type_) {
-            case Type::ReservedWord:
-                what_ += "reserved word";
-                break;
-            case Type::Identifier:
-                what_ += "identifier";
-                break;
-            case Type::Operator:
-                what_ += "operator";
-                break;
-            case Type::Dot:
-                what_ += "dot";
-                break;
-            case Type::Comma:
-                what_ += "comma";
-                break;
-            case Type::Punctuation:
-                what_ += "bracket";
-                break;
-        }
+        generateWhat();
     }
 
     Token(std::string &&content, size_t line, size_t col, Type type) : std::exception(),
                                                                     line_(line), column_(col),
                                                                     type_(type),
                                                                     content_(std::move(content)) {
-        what_ = "in " + std::to_string(line_) + ":" + std::to_string(column_);
-        what_ += " error: unexpected token: \"" + content_ + "\" with type: ";
-        switch (type_) {
-            case Type::ReservedWord:
-                what_ += "reserved word";
-                break;
-            case Type::Identifier:
-                what_ += "identifier";
-                break;
-            case Type::Operator:
-                what_ += "operator";
-                break;
-            case Type::Dot:
-                what_ += "dot";
-                break;
-            case Type::Comma:
-                what_ += "comma";
-                break;
-            case Type::Punctuation:
-                what_ += "bracket";
-                break;
-        }
+        generateWhat();
     }
 
     [[nodiscard]]
@@ -83,4 +42,31 @@ class Token : public std::exception {
     size_t line_, column_;
     Type type_;
     std::string content_, what_;
+
+    void generateWhat() {
+        what_ = "in " + std::to_string(line_) + ":" + std::to_string(column_);
+        what_ += " error: unexpected token: \"" + content_ + "\" with type: ";
+        switch (type_) {
+            case Type::ReservedWord:
+                what_ += "reserved word";
+                break;
+            case Type::Identifier:
+                what_ += "identifier";
+                break;
+            case Type::Operator:
+                what_ += "operator";
+                break;
+            case Type::Dot:
+                what_ += "dot";
+                break;
+            case Type::Comma:
+                what_ += "comma";
+                break;
+            case Type::Punctuation:
+                what_ += "bracket";
+                break;
+            case Type::Literal:
+                what_ += "literal";
+        }
+    }
 };
