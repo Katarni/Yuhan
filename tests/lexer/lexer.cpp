02ed74ebc8ad9e0bd7dca15be549e615b9fc6975
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-10-29 21:51:55 +0500

    start working with tests

---
+++ tests/lexer/lexer.cpp
@@ -0,0 +1,18 @@
+#include "include/Starter.h"
+
+int main() {
+    std::string file_path;
+    std::cin >> file_path;
+
+    std::ifstream file_fin(file_path);
+    file_fin.seekg(0, std::ios::end);
+    std::streamsize file_size = file_fin.tellg();
+    file_fin.seekg(0, std::ios::beg);
+
+    auto file_text = new char[file_size];
+    file_fin.read(file_text, file_size);
+
+    Starter starter(file_text, file_size, "../../../reserved-words.txt");
+
+    return 0;
+}
