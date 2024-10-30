e54f61f6890dbd5a803691b93b8fb387868872ba
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-10-30 13:52:36 +0500

    test output added

--- tests/lexer/lexer.cpp
+++ tests/lexer/lexer.cpp
@@ -1,4 +1,4 @@
-#include "include/Starter.h"
+#include "../../include/Starter.h"
 
 int main() {
     std::string file_path;
@@ -13,6 +13,37 @@ int main() {
     file_fin.read(file_text, file_size);
 
     Starter starter(file_text, file_size, "../../../reserved-words.txt");
+
+    auto tokens = starter.getAllTokens();
+
+    std::map < Token::Type, std::string > descriptions = {
+            {Token::Type::ReservedWord, "reserved word"},
+            {Token::Type::Identifier, "identifier"},
+            {Token::Type::LvalueBinaryOperator, "lvalue binary operator"},
+            {Token::Type::RvalueBinaryOperator, "rvalue binary operator"},
+            {Token::Type::Dot, "dot"},
+            {Token::Type::Comma, "comma"},
+            {Token::Type::NumericLiteral, "numeric literal"},
+            {Token::Type::Semicolon, "semicolon"},
+            {Token::Type::OpenParenthesis, "open parenthesis"},
+            {Token::Type::CloseParenthesis, "close parenthesis"},
+            {Token::Type::OpenCurlyBrace, "open curly brace"},
+            {Token::Type::CloseCurlyBrace, "close curly brace"},
+            {Token::Type::OpenSquareBracket, "open square bracket"},
+            {Token::Type::CloseSquareBracket, "close square bracket"},
+            {Token::Type::LvalueUnaryOperator, "lvalue unary operator"},
+            {Token::Type::RvalueUnaryOperator, "rvalue unary operator"},
+            {Token::Type::StringLiteral, "string literal"},
+            {Token::Type::EndOfFile, "endl of file"},
+            {Token::Type::Another, "another"},
+            {Token::Type::ExponentialLiteral, "exponential literal"},
+            {Token::Type::PlusOrMinusOperator, "plus or minus operator"},
+            {Token::Type::FloatLiteral, "float literal"}
+    };
+
+    for (auto& token : tokens) {
+        std::cout << descriptions[token.getType()] << ' ' << token.getContent() << ' ' << token.getLine() << ':' << token.getColumn() << std::endl;
+    }
 
     return 0;
 }
