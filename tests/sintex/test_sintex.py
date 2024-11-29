2a044a66e6e920ab10ca887741eee3310f1472c0
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-11-29 09:33:10 +0300

    .txt to .y in tests

--- tests/sintex/test_sintex.py
+++ tests/sintex/test_sintex.py
@@ -3,11 +3,11 @@ from subprocess import run
 
 
 def get_program_output():
-    return run("build/Yuhan-sintex-tests", encoding="utf-8", capture_output=True, input="sintex-test.txt").stdout
+    return run("build/Yuhan-sintex-tests", encoding="utf-8", capture_output=True, input="sintex-test.y").stdout
 
 
 def write_file(input_text):
-    file = open("sintex-test.txt", "w")
+    file = open("sintex-test.y", "w")
     file.write(input_text)
     file.close()
 
@@ -20,42 +20,42 @@ def read_file(input_file):
 
 
 def test_structs():
-    file = read_file("inputs/structs.txt")
+    file = read_file("inputs/structs.y")
     write_file(file)
 
     assert get_program_output() == "OK"
 
 
 def test_literals():
-    file = read_file("inputs/literals.txt")
+    file = read_file("inputs/literals.y")
     write_file(file)
 
     assert get_program_output() == "OK"
 
 
 def test_cycles():
-    file = read_file("inputs/cycles.txt")
+    file = read_file("inputs/cycles.y")
     write_file(file)
 
     assert get_program_output() == "OK"
     
 
 def test_if_else():
-    file = read_file("inputs/if-else.txt")
+    file = read_file("inputs/if-else.y")
     write_file(file)
 
     assert get_program_output() == "OK"
 
 
 def test_namespaces():
-    file = read_file("inputs/namespaces.txt")
+    file = read_file("inputs/namespaces.y")
     write_file(file)
 
     assert get_program_output() == "OK"
 
 
 def test_functions():
-    file = read_file("inputs/functions.txt")
+    file = read_file("inputs/functions.y")
     write_file(file)
 
     assert get_program_output() == "OK"
